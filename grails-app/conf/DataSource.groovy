3c8a1c07217ed605d383418de27f0b51afc15645
Eric Field <ef130968@gmail.com>
2011-10-07 19:33:41 -0400

    Up and running with new MySQL database instance 'EricFieldIs_development'

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,32 +1,33 @@
 dataSource {
     pooled = true
-    driverClassName = "org.hsqldb.jdbcDriver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    username = "root"
+    password = "root"
 }
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = true
-    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+    cache.provider_class = "net.sf.ehcache.hibernate.EhCacheProvider"
 }
+
 // environment specific settings
 environments {
     development {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-            url = "jdbc:hsqldb:mem:devDB"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:hsqldb:mem:testDb"
-        }
-    }
-    production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
-        }
-    }
+		    dataSource {
+			      dbCreate = "update" // one of 'create', 'create-drop','update'
+			      url = "jdbc:mysql://localhost:3306/EricFieldIs_development"
+		    }
+	  }
+	  test {
+		    dataSource {
+			      dbCreate = "update"
+			      url = "jdbc:mysql://localhost:3306/EricFieldIs_test"
+		    }
+	  }
+	  production {
+		    dataSource {
+			      dbCreate = "update"
+			      url = "jdbc:mysql://localhost:3306/EricFieldIs_production"
+		    }
+	  }
 }
---
+++ grails-app/views/home/main.gsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: efield
+  Date: 07/10/11
+  Time: 1:47 PM
+  To change this template use File | Settings | File Templates.
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+  <head><title>Eric Field Is Me</title></head>
+  <body>Eric Field is...
+  Me
+  Design
+  Imagination
+  </body>
+</html>
